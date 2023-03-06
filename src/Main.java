7f6a819e90e53fc03cd4b04c86ede16e633c994e
MikalaiKholin <kholin.mikalai@gmail.com>
2023-03-06 23:21:14 +0300

    refactor: changed the description for a % b

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.printf("a - b = %d%n", a - b);
         System.out.printf("a / b = %d%n", a / b);
         System.out.printf("a * b = %d%n", a * b);
-        System.out.printf("the remainder of the division a and b %d%n", a % b);
+        System.out.printf("the remainder of the division a and b is %d%n", a % b);
         System.out.printf("is a an even number? %b%n", a % 2 == 0);
         System.out.printf("is b an even number? %b%n", b % 2 == 0);
         System.out.print("😀");
